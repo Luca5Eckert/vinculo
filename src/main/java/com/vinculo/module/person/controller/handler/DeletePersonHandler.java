f638d8b796ccb991a4dad4880ea02c6a1251bf92
Luca5Eckert <lucasismaeleckert@gmail.com>
2026-02-10 19:15:23 -0300

    feat: add DeletePersonHandler

---
+++ src/main/java/com/vinculo/module/person/controller/handler/DeletePersonHandler.java
@@ -0,0 +1,26 @@
+package com.vinculo.module.person.controller.handler;
+
+import com.vinculo.module.person.domain.command.DeletePersonCommand;
+import com.vinculo.module.person.domain.use_case.DeletePersonUseCase;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+public class DeletePersonHandler {
+
+    private final DeletePersonUseCase deletePersonUseCase;
+
+    public DeletePersonHandler(DeletePersonUseCase deletePersonUseCase) {
+        this.deletePersonUseCase = deletePersonUseCase;
+    }
+
+    @Transactional
+    public void handle(Long personId){
+        DeletePersonCommand command = new DeletePersonCommand(
+                personId
+        );
+
+        deletePersonUseCase.execute(command);
+    }
+
+}
