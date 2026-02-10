22552180bd7f14ecdba694c5adb21a15da87ebef
Luca5Eckert <lucasismaeleckert@gmail.com>
2026-02-10 19:28:15 -0300

    feat: add endpoint to update user

--- src/main/java/com/vinculo/module/person/controller/controller/PersonController.java
+++ src/main/java/com/vinculo/module/person/controller/controller/PersonController.java
@@ -2,11 +2,14 @@ package com.vinculo.module.person.controller.controller;
 
 import com.vinculo.module.person.controller.dto.CreatePersonRequest;
 import com.vinculo.module.person.controller.dto.PersonResponse;
+import com.vinculo.module.person.controller.dto.UpdatePersonRequest;
 import com.vinculo.module.person.controller.handler.CreatePersonHandler;
 import com.vinculo.module.person.controller.handler.DeletePersonHandler;
 import com.vinculo.module.person.controller.handler.GetPersonHandler;
+import com.vinculo.module.person.controller.handler.UpdatePersonHandler;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,11 +20,13 @@ public class PersonController {
     private final CreatePersonHandler createPersonHandler;
     private final DeletePersonHandler deletePersonHandler;
     private final GetPersonHandler getPersonHandler;
+    private final UpdatePersonHandler updatePersonHandler;
 
-    public PersonController(CreatePersonHandler createPersonHandler, DeletePersonHandler deletePersonHandler, GetPersonHandler getPersonHandler) {
+    public PersonController(CreatePersonHandler createPersonHandler, DeletePersonHandler deletePersonHandler, GetPersonHandler getPersonHandler, UpdatePersonHandler updatePersonHandler) {
         this.createPersonHandler = createPersonHandler;
         this.deletePersonHandler = deletePersonHandler;
         this.getPersonHandler = getPersonHandler;
+        this.updatePersonHandler = updatePersonHandler;
     }
 
     @PostMapping
@@ -34,6 +39,7 @@ public class PersonController {
     }
 
     @DeleteMapping("/{personId}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Void> delete(@PathVariable(value = "personId") Long personId){
         deletePersonHandler.handle(personId);
 
@@ -47,6 +53,18 @@ public class PersonController {
         PersonResponse response = getPersonHandler.handle(personId);
 
         return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/{personId}")
+    public ResponseEntity<Void> update(
+            @PathVariable Long personId,
+            @Validated @RequestBody UpdatePersonRequest request
+    ) {
+        updatePersonHandler.handle(personId, request);
+
+        return ResponseEntity
+                .status(HttpStatus.NO_CONTENT)
+                .build();
     }
 
 }
