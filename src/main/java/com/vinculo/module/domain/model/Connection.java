15967270105e9ecc922aa65450d07906562f514b
Luca5Eckert <lucasismaeleckert@gmail.com>
2026-02-10 18:47:24 -0300

    feat: add Connection

--- src/main/java/com/vinculo/module/domain/model/Connection.java
+++ src/main/java/com/vinculo/module/domain/model/Connection.java
@@ -1,13 +1,30 @@
 package com.vinculo.module.domain.model;
 
-import org.springframework.data.neo4j.core.schema.Relationship;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.neo4j.core.schema.RelationshipId;
 import org.springframework.data.neo4j.core.schema.RelationshipProperties;
+import org.springframework.data.neo4j.core.schema.TargetNode;
 
 @RelationshipProperties
+@Setter
+@Getter
 public class Connection {
 
     @RelationshipId
     private Long id;
+
+    @TargetNode
+    private Person person;
+
+    private TypeConnection type;
+
+    public Connection() {
+    }
+
+    public Connection(Person person, TypeConnection type) {
+        this.person = person;
+        this.type = type;
+    }
 
 }
