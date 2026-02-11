781864864f211be69683c329f0a4531dfcc86692
Luca5Eckert <lucasismaeleckert@gmail.com>
2026-02-10 21:19:02 -0300

    feat: add endpoint to get all connection of user

--- src/main/java/com/vinculo/module/connection/application/controller/ConnectionController.java
+++ src/main/java/com/vinculo/module/connection/application/controller/ConnectionController.java
@@ -1,23 +1,30 @@
 package com.vinculo.module.connection.application.controller;
 
+import com.vinculo.module.connection.application.dto.ConnectionResponse;
 import com.vinculo.module.connection.application.dto.CreateConnectionRequest;
 import com.vinculo.module.connection.application.handler.CreateConnectionHandler;
+import com.vinculo.module.connection.application.handler.GetMyConnectionsHandler;
 import com.vinculo.share.service.AuthenticationService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/connections")
 public class ConnectionController {
 
     private final CreateConnectionHandler createConnectionHandler;
+    private final GetMyConnectionsHandler getMyConnectionsHandler;
 
     private final AuthenticationService authenticationService;
 
-    public ConnectionController(CreateConnectionHandler createConnectionHandler, AuthenticationService authenticationService) {
+    public ConnectionController(CreateConnectionHandler createConnectionHandler, GetMyConnectionsHandler getMyConnectionsHandler, AuthenticationService authenticationService) {
         this.createConnectionHandler = createConnectionHandler;
+        this.getMyConnectionsHandler = getMyConnectionsHandler;
         this.authenticationService = authenticationService;
     }
 
@@ -28,6 +35,15 @@ public class ConnectionController {
         createConnectionHandler.handle(request, personId);
 
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<List<ConnectionResponse>> getMyConnections(){
+        long personId = authenticationService.getAuthenticatedPersonId();
+
+        var response = getMyConnectionsHandler.handle(personId);
+
+        return ResponseEntity.ok(response);
     }
 
 }
