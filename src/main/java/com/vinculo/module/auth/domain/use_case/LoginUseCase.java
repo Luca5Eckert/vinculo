b9d1e5cdf576b9f6c9a4f5e989c3be06e98ea05c
Luca5Eckert <lucasismaeleckert@gmail.com>
2026-02-11 15:43:26 -0300

    feat: add LoginUseCase

---
+++ src/main/java/com/vinculo/module/auth/domain/use_case/LoginUseCase.java
@@ -0,0 +1,28 @@
+package com.vinculo.module.auth.domain.use_case;
+
+import com.vinculo.module.auth.domain.model.AuthenticatedUser;
+import com.vinculo.module.auth.domain.port.AuthenticatorPort;
+import com.vinculo.module.auth.domain.port.TokenProvider;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LoginUseCase {
+
+    private final AuthenticatorPort authenticatorPort;
+    private final TokenProvider tokenProvider;
+
+    public LoginUseCase(AuthenticatorPort authenticatorPort, TokenProvider tokenProvider) {
+        this.authenticatorPort = authenticatorPort;
+        this.tokenProvider = tokenProvider;
+    }
+
+    public String execute(String email, String password) {
+        AuthenticatedUser user = authenticatorPort.authenticate(email, password);
+
+        return tokenProvider.createToken(
+                user.email(),
+                user.id(),
+                user.roles()
+        );
+    }
+}
