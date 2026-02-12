e126298ffaecf93ad183847c456dafb28b8b4921
Luca5Eckert <lucasismaeleckert@gmail.com>
2026-02-12 16:07:56 -0300

    fix: add @Component in RequestConnectionMapper

--- src/main/java/com/vinculo/module/request_connection/application/mapper/RequestConnectionMapper.java
+++ src/main/java/com/vinculo/module/request_connection/application/mapper/RequestConnectionMapper.java
@@ -2,7 +2,9 @@ package com.vinculo.module.request_connection.application.mapper;
 
 import com.vinculo.module.request_connection.application.dto.RequestConnectionResponse;
 import com.vinculo.module.request_connection.domain.model.RequestConnection;
+import org.springframework.stereotype.Component;
 
+@Component
 public class RequestConnectionMapper {
     public RequestConnectionResponse toResponse(RequestConnection requestConnection) {
         return new RequestConnectionResponse(
