b91b92d35eddf203ac9af28b41d72d81f1f2d989
Luca5Eckert <lucasismaeleckert@gmail.com>
2026-02-12 15:49:40 -0300

    feat: add endpoints to get my request connections

--- src/main/java/com/vinculo/module/request_connection/application/controller/RequestConnectionController.java
+++ src/main/java/com/vinculo/module/request_connection/application/controller/RequestConnectionController.java
@@ -1,8 +1,10 @@
 package com.vinculo.module.request_connection.application.controller;
 
 import com.vinculo.module.auth.domain.model.AuthenticatedUser;
+import com.vinculo.module.request_connection.application.dto.RequestConnectionResponse;
 import com.vinculo.module.request_connection.application.dto.SendRequestConnectionRequest;
 import com.vinculo.module.request_connection.application.dto.UpdateStatusRequestConnectionRequest;
+import com.vinculo.module.request_connection.application.handler.GetMyRequestConnectionsHandler;
 import com.vinculo.module.request_connection.application.handler.SendRequestConnectionHandler;
 import com.vinculo.module.request_connection.application.handler.UpdateStatusRequestConnectionHandler;
 import com.vinculo.share.service.AuthenticationService;
@@ -11,18 +13,22 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/v1/request-connections")
 public class RequestConnectionController {
 
     private final SendRequestConnectionHandler sendRequestConnectionHandler;
     private final UpdateStatusRequestConnectionHandler updateStatusRequestConnectionHandler;
+    private final GetMyRequestConnectionsHandler getMyRequestConnectionsHandler;
 
     private final AuthenticationService authenticationService;
 
-    public RequestConnectionController(SendRequestConnectionHandler sendRequestConnectionHandler, UpdateStatusRequestConnectionHandler updateStatusRequestConnectionHandler, AuthenticationService authenticationService) {
+    public RequestConnectionController(SendRequestConnectionHandler sendRequestConnectionHandler, UpdateStatusRequestConnectionHandler updateStatusRequestConnectionHandler, GetMyRequestConnectionsHandler getMyRequestConnectionsHandler, AuthenticationService authenticationService) {
         this.sendRequestConnectionHandler = sendRequestConnectionHandler;
         this.updateStatusRequestConnectionHandler = updateStatusRequestConnectionHandler;
+        this.getMyRequestConnectionsHandler = getMyRequestConnectionsHandler;
         this.authenticationService = authenticationService;
     }
 
@@ -30,7 +36,7 @@ public class RequestConnectionController {
     public ResponseEntity<Void> send(
             @PathVariable(value = "personTargetId") Long personTargetId,
             @Validated @RequestBody SendRequestConnectionRequest request
-            ) {
+    ) {
         long personRequesterId = authenticationService.getAuthenticatedPersonId();
 
         sendRequestConnectionHandler.handle(personRequesterId, personTargetId, request);
@@ -56,6 +62,15 @@ public class RequestConnectionController {
         return ResponseEntity
                 .status(HttpStatus.NO_CONTENT)
                 .build();
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<List<RequestConnectionResponse>> getMyRequestConnections() {
+        long personId = authenticationService.getAuthenticatedPersonId();
+
+        var requestConnections = getMyRequestConnectionsHandler.handle(personId);
+
+        return ResponseEntity.ok(requestConnections);
     }
 
 }
